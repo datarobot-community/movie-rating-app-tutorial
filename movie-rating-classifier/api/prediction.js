443da3ea86b0c6747c40cb7528f8f4a66eba02b9
Zan Markan <zan.markan@datarobot.com>
2020-05-14 17:59:38 +0100

    Add prediction code in the api

--- movie-rating-classifier/api/prediction.js
+++ movie-rating-classifier/api/prediction.js
@@ -1,6 +1,24 @@
 const unirest = require("unirest");
+const env = require("./_env")
 
 module.exports = async (req, res) => {
-    //TODO: implement DataRobot prediction code
-    res.send({})
+    let review = req.body.review
+    console.log("Predicting review: ")
+    console.log(review)
+
+    let response = await unirest.post(`${env.PREDICTION_SERVER}/predApi/v1.0/deployments/${env.DEPLOYMENT_ID}/predictions`)
+    .headers({
+        "Authorization": `Bearer ${env.API_KEY}`,
+        "datarobot-key": env.DATAROBOT_KEY
+    })
+    .type('json')
+    .send(
+        [{
+        "review": review
+    }]
+    )
+    console.log("Got response:")
+    console.log(response.body.data)
+    
+    res.send({prediction: response.body.data[0].prediction})
 }
